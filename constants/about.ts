5fbde1fad75b6f26d8796387c7b344c2f02d7669
Lav Kushwaha <lavkumar1224@gmail.com>
2026-04-16 13:32:10 +0530

    constants/about.ts

---
+++ constants/about.ts
@@ -0,0 +1,11 @@
+export const aboutData = {
+  bio: "Hi, I’m Lav Kushwaha, a Full Stack Developer specializing in building robust and scalable web applications. With a strong foundation in both front-end and back-end technologies, I create seamless user experiences and efficient server-side solutions. I’m passionate about learning new technologies, solving complex problems, and turning innovative ideas into reality. When I’m not coding, I enjoy exploring tech trends.",
+  interests: ["Coding", "reading", "content creation"],
+  contact: {
+    email: "lavkushwahaa@gmail.com",
+    linkedin: "https://linkedin.com/in/lavkushwaha",
+    github: "https://github.com/lav-kushwaha",
+    twitter: "https://twitter.com/lavkushwaha_",
+  },
+  images: ["/Profile/profile.jpg",],
+};
