94222380941249dde7be363e34f2bec0c7a7a781
Lav Kushwaha <lavkumar1224@gmail.com>
2026-04-17 12:47:55 +0530

    Updated skills section

---
+++ constants/skills.ts
@@ -0,0 +1,60 @@
+const DI = "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons";
+
+export const skillsData = [
+  {
+    _id: "1",
+    category: "Frontend",
+    skills: [
+      { _id: "1",  name: "HTML",     icon: `${DI}/html5/html5-original.svg` },
+      { _id: "2",  name: "CSS",      icon: `${DI}/css3/css3-original.svg` },
+      { _id: "3",  name: "Tailwind", icon: `${DI}/tailwindcss/tailwindcss-original.svg` },
+      { _id: "4",  name: "React",    icon: `${DI}/react/react-original.svg` },
+      { _id: "5",  name: "Next.js",  icon: `${DI}/nextjs/nextjs-original.svg` },
+    ],
+  },
+  {
+    _id: "2",
+    category: "Backend",
+    skills: [
+      { _id: "6", name: "Node.js", icon: `${DI}/nodejs/nodejs-original.svg` },
+      { _id: "7", name: "Express", icon: `${DI}/express/express-original.svg` },
+    ],
+  },
+  {
+    _id: "3",
+    category: "Databases",
+    skills: [
+      { _id: "8",  name: "MongoDB",              icon: `${DI}/mongodb/mongodb-original.svg` },
+      { _id: "9",  name: "MySQL",                icon: `${DI}/mysql/mysql-original.svg` },
+      { _id: "10", name: "PostgreSQL",           icon: `${DI}/postgresql/postgresql-original.svg` },
+      { _id: "11", name: "Microsoft SQL Server", icon: `${DI}/microsoftsqlserver/microsoftsqlserver-original.svg` },
+    ],
+  },
+  {
+    _id: "4",
+    category: "Languages",
+    skills: [
+      { _id: "12", name: "JavaScript", icon: `${DI}/javascript/javascript-original.svg` },
+      { _id: "13", name: "Java",       icon: `${DI}/java/java-original.svg` },
+      { _id: "14", name: "TypeScript", icon: `${DI}/typescript/typescript-original.svg` },
+    ],
+  },
+  {
+    _id: "5",
+    category: "Tools",
+    skills: [
+      { _id: "15", name: "Git",     icon: `${DI}/git/git-original.svg` },
+      { _id: "16", name: "Postman", icon: `${DI}/postman/postman-original.svg` },
+      { _id: "17", name: "VS Code", icon: `${DI}/vscode/vscode-original.svg` },
+    ],
+  },
+  {
+    _id: "6",
+    category: "Media",
+    skills: [
+      { _id: "18", name: "Premiere Pro",  icon: `${DI}/premierepro/premierepro-original.svg` },
+      { _id: "19", name: "After Effects", icon: `${DI}/aftereffects/aftereffects-original.svg` },
+      { _id: "20", name: "Photoshop",     icon: `${DI}/photoshop/photoshop-original.svg` },
+    ],
+  },
+];
