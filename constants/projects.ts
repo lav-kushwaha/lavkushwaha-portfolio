e2c90fdac0e8ca1ec046268cadc39073a7588b32
Lav Kushwaha <lavkumar1224@gmail.com>
2026-04-17 12:48:53 +0530

    projects

---
+++ constants/projects.ts
@@ -0,0 +1,110 @@
+export const projects = [
+  {
+    _id: "1",
+    title: "Ecommerce Website",
+    description:
+      "This is a comprehensive full-stack eCommerce application built using the MERN Stack (MongoDB, Express.js, React.js, Node.js). The project features full user and admin functionalities, secure authentication, product management, PayPal integration, and a clean, responsive UI with modern UX best practices.",
+    techStack: ["React", "Tailwind", "ShadCN", "Node", "Express", "MongoDB"],
+    thumbnail: "/projects/ecommerce.jpg",
+    links: {
+      live: "https://mern-ecommerce-store-1-66zv.onrender.com/",
+      github: "https://github.com/lav-kushwaha/mern-ecommerce-store",
+    },
+  },
+  {
+    _id: "2",
+    title: "DevTinder",
+    description:
+      "DevTinder is a full-stack social networking platform tailored for developers. Think of it as Tinder for Devs — where developers can discover, connect, and collaborate. Users can create profiles, explore others, send connection requests, chat, and manage their profile — all in a smooth and interactive interface.",
+    techStack: ["React", "Tailwind", "Socket io", "Node", "Express", "MongoDB"],
+    thumbnail: "/projects/devtinder.png",
+    links: {
+      live: "https://developer-tinder.vercel.app",
+      github: "https://github.com/lav-kushwaha/Complete-Node-JS",
+    },
+  },
+  {
+    _id: "3",
+    title: "Personal Portfolio Using MERN STACK",
+    description:
+      "A full-stack personal portfolio website built with the MERN stack (MongoDB, Express.js, React, Node.js). Showcases my projects, skills, and experience.",
+    techStack: ["React", "Tailwind", "Node", "Express", "MongoDB"],
+    thumbnail: "/projects/portfolio.png",
+    links: {
+      live: "https://lavkushwaha.vercel.app",
+      github: "",
+    },
+  },
+  {
+    _id: "4",
+    title: "Digital Network House",
+    description:
+      "The Digital Network House website is a responsive single-page application built with React.js and Tailwind CSS, emphasizing clean component architecture, reusability, and optimized performance. It delivers a smooth user experience with fast navigation, minimal load times, and scalable front-end structure.",
+    techStack: ["React", "Tailwind"],
+    thumbnail: "/projects/digital-network.png",
+    links: {
+      live: "https://www.digitalnetworkhouse.in",
+      github: "",
+    },
+  },
+  {
+    _id: "5",
+    title: "GPT-3",
+    description:
+      "Built a modern, responsive landing page showcasing GPT-3’s features and applications. Focused on clean UI, smooth navigation, and mobile-friendly design. Implemented sections for features, blog, and calls-to-action using React. The site highlights my skills in front-end development, responsive layouts, and creating engaging user experiences.",
+    techStack: ["React", "Tailwind", "Netlify"],
+    thumbnail: "/projects/gpt3.png",
+    links: {
+      live: "https://lavgpt.netlify.app",
+      github: "https://github.com/lav-kushwaha/React-Js-GPT3_Modern-Website",
+    },
+  },
+  {
+    _id: "6",
+    title: "Youtube Clone",
+    description:
+      "A YouTube-like video sharing platform built using the MERN stack. This application allows users to upload, view, search, like, and comment on videos with secure user authentication and a responsive UI.",
+    techStack: ["React", "Tailwind", "Node", "Express", "MongoDB"],
+    thumbnail: "/projects/youtube.png",
+    links: {
+      live: "",
+      github: "https://github.com/lav-kushwaha/YouTube-Clone-Using-MERN-STACK",
+    },
+  },
+  {
+    _id: "7",
+    title: "IMDB CLONE",
+    description:
+      "Developed an IMDB Clone Movie Website using React.js and the TMDB API, featuring dynamic movie data, interactive Movie Cards, and detailed Movie Details pages. Implemented a movie carousel, trending/new movie filters, and a responsive, user-friendly interface using CSS, JavaScript, and Material UI.",
+    techStack: ["React", "Tailwind", "TMDB API"],
+    thumbnail: "/projects/imdb.png",
+    links: {
+      live: "https://imdbmoviez.vercel.app/",
+      github: "https://github.com/lav-kushwaha/IMDB-Clone",
+    },
+  },
+  {
+    _id: "8",
+    title: "Inventyv Clone Frontend Website",
+    description:
+      "Developed a responsive frontend clone of the Inventyv website using React.js, CSS, and Material UI. The project replicates the platform’s modern interface with smooth navigation, dynamic components, and clean design, showcasing strong front-end development and UI design skills.",
+    techStack: ["React", "Tailwind"],
+    thumbnail: "/projects/inventy.png",
+    links: {
+      live: "https://inventyv.netlify.app/",
+      github: "https://github.com/lav-kushwaha/Inventyv-Website",
+    },
+  },
+  {
+    _id: "9",
+    title: "Prominent Pixel Clone Frontend Website",
+    description:
+      "Built a responsive clone of the Prominent Pixel website using React.js, CSS, and Material UI. Recreated the platform’s professional layout and interactive UI elements with a focus on clean design, smooth performance, and modern front-end development practices.",
+    techStack: ["React", "Tailwind"],
+    thumbnail: "/projects/prominent.png",
+    links: {
+      live: "https://prominentpixel.netlify.app/",
+      github: "https://github.com/lav-kushwaha/ProminentPixel",
+    },
+  },
+];
